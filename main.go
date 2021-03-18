dc99d8ad05d9f96cb99d638021e9810b5aed3bf2
Gary <gongxicheng1111@gmail.com>
2021-03-18 21:53:18 +0800

    add

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+
+	b := 12
+
+	fmt.Println(b, 123, 123, 123, 123)
+	typeofb := reflect.TypeOf(b)
+
+	fmt.Println(typeofb)
 
 	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j, s = i+1, j+1, s+"a" {
 		fmt.Println("Value of i, j, s:", i, j, s)
